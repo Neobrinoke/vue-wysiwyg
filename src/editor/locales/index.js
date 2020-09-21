bdc6affb9ba26ce4a60e3a17d5041319f7d8546b
Neobrinoke <neobrinoke@gmail.com>
2020-09-21 14:18:13 +0200

    Fix german import

--- src/editor/locales/index.js
+++ src/editor/locales/index.js
@@ -2,8 +2,8 @@ import dutch from "./dutch";
 import hungarian from "./hungarian";
 import english from "./english";
 import portuguese from "./portuguese";
-import italian from "./italian";
 import spanish from "./spanish";
+import german from "./german";
 
 export default {
     dutch,
